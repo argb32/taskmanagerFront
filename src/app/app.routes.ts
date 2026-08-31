c6081d78329ddfac6318c9d804db2eb6c6a0a0ab
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-08-31 11:16:14 +0100

    activar botones del layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./tasks-front/task-front.routes'),
+  },
+  {
     path: 'tasks',
+    loadChildren: () => import('./tasks-front/task-front.routes'),
+  },
+  {
+    path: 'users',
     loadChildren: () => import('./tasks-front/task-front.routes'),
   },
 ];
--- src/app/tasks-front/components/layout/layout.ts
+++ src/app/tasks-front/components/layout/layout.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  RouterOutlet,
+  RouterLinkWithHref,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.html',
 })
 export class Layout {}
